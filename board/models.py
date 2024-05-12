64dae1057bddb85b722b0875ecca00ec03cab409
ve4nick <ve4nick98@gmail.com>
2024-05-12 20:24:33 +0300

    Created Position model

--- board/models.py
+++ board/models.py
@@ -10,3 +10,13 @@ class TaskType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
