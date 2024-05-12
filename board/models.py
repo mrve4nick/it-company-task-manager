ce6cd24eb0156965386c3872681403b398dee1ac
ve4nick <ve4nick98@gmail.com>
2024-05-12 20:29:16 +0300

    Created Task model and PRIORITY_CHOICES for priority field in Task model

--- board/models.py
+++ board/models.py
@@ -25,3 +25,25 @@ class Position(models.Model):
 
 class Worker(AbstractUser):
     position = models.ManyToManyField(Position, blank=False, related_name="workers")
+
+
+PRIORITY_CHOICES = (
+    ("urgent", "URGENT PRIORITY"),
+    ("high", "HIGH PRIORITY"),
+    ("medium", "MEDIUM PRIORITY"),
+    ("low", "LOW PRIORITY"),
+)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=6,
+        choices=PRIORITY_CHOICES,
+        default="urgent",
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="assigned_tasks", blank=True)
